964ac55e4d9cf6718350caf3cec24b1daa28176e
phpuser2017 <178675994@qq.com>
2018-11-05 18:04:40 +0800

    1105

--- components/zan/index.js
+++ components/zan/index.js
@@ -4,14 +4,18 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like:{
+      type:Boolean
+    },
+    count:{
+      type:Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
   },
 
   /**
@@ -19,8 +23,11 @@ Component({
    */
   methods: {
     taplike:function(e){
-      console.log(e)
-      console.log(e.currentTarget.dataset.code)
+      let count = this.properties.like ? this.properties.count - 1 : this.properties.count + 1
+      this.setData({
+        like: !this.properties.like,
+        count: count
+      })
     }
   }
 })
